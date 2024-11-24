cf74e7514e4dd624d43c11718b345d01ac777e99
Guang_Chen_ <guangchenworks@outlook.com>
2024-11-24 14:02:46 +0800

    fix: Router problem

--- src/app/services/word.service.ts
+++ src/app/services/word.service.ts
@@ -1,14 +1,15 @@
 import {Injectable} from '@angular/core';
 import {IWordDetail} from '../interfaces/word-detail.interface';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, map, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordService {
-  url = 'https://raw.githubusercontent.com/GuangChen2333/CanWeTalkSimplyData/refs/heads/database/db.json';
-  words: IWordDetail[] = []
+  private url = 'https://raw.githubusercontent.com/GuangChen2333/CanWeTalkSimplyData/refs/heads/database/db.json';
+  private wordsSubject: BehaviorSubject<IWordDetail[]> = new BehaviorSubject<IWordDetail[]>([]);
+  private words: Observable<IWordDetail[]> = this.wordsSubject.asObservable();
 
   constructor(private http: HttpClient) {
   }
@@ -17,10 +18,9 @@ export class WordService {
     return this.http.get<IWordDetail[]>(this.url)
   }
 
-
   fetchWords(): void {
     this.getWords().subscribe(words => {
-      this.words = words;
+      this.wordsSubject.next(words);
     })
   }
 
@@ -45,33 +45,36 @@ export class WordService {
     return d[a.length][b.length];
   }
 
-  filterByName(name: string): IWordDetail[] {
-    let distances: {
-      [id: number]: number
-    } = {}
+  filterByName(name: string): Observable<IWordDetail[]> {
+    return this.words.pipe(
+      map(
+        (words: IWordDetail[]) => {
+          let distances: {
+            [id: number]: number
+          } = {}
 
-    for (let word of this.words) {
-      distances[word.id] = this.levenshteinDistance(name, word.name)
-    }
+          for (let word of this.wordsSubject.value) {
+            distances[word.id] = this.levenshteinDistance(name, word.name)
+          }
 
-    console.info(distances);
-
-    return this.words
-      .filter(
-        word => name.split("").some(char => word.name.includes(char))
-      )
-      .map(
-        (word) => (
-          {...word, distance: distances[word.id]}
-        )
-      )
-      .sort(
-        (a, b) => {
-          if (a.name === name) return -1;
-          if (b.name === name) return 1;
-          if (a.distance !== b.distance) return a.distance - b.distance;
-          return a.id - b.id
-        }
-      )
+          return words
+            .filter(
+              word => name.split("").some(char => word.name.includes(char))
+            )
+            .map(
+              (word) => (
+                {...word, distance: distances[word.id]}
+              )
+            )
+            .sort(
+              (a, b) => {
+                if (a.name === name) return -1;
+                if (b.name === name) return 1;
+                if (a.distance !== b.distance) return a.distance - b.distance;
+                return a.id - b.id
+              }
+            )
+        })
+    )
   }
 }
